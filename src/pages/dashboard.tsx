0c5799a236070216aab88a61faf06c60a7de9762
Mohammad Azmi <38707148+azmy60@users.noreply.github.com>
2022-09-26 16:01:02 +0700

    Save notes to db for authenticated users

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,17 +1,28 @@
 import { PlusIcon } from "@heroicons/react/24/solid";
+import { Note } from "@prisma/client";
+import moment from "moment";
 import type { NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
-
-const Notes = [
-  {
-    title: "My first note",
-  },
-];
+import Router from "next/router";
+import { useEffect, useState } from "react";
 
 const Dashboard: NextPage = () => {
   const { data: session } = useSession();
+
+  const [notes, setNotes] = useState([] as Note[]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/notes")
+      .then((res) => res.json())
+      .then((notes: Note[]) => {
+        setNotes(notes);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <>
@@ -29,15 +40,32 @@ const Dashboard: NextPage = () => {
           <button onClick={() => signOut()} className="btn btn-outline">
             Sign out
           </button>
-          <button className="btn btn-primary gap-2">
+          <button
+            onClick={async () => {
+              const res = await fetch("/api/note");
+              const { id } = await res.json();
+              Router.push(`/note/${id}`);
+            }}
+            className="btn btn-primary gap-2"
+          >
             <PlusIcon className="w-6 h-6" /> New note
           </button>
         </div>
         <div className="mt-12 grid grid-cols-3 gap-8">
-          {Notes.map((note) => (
-            <Link href="/">
+          {loading && (
+            <div
+              className="radial-progress animate-spin"
+              style={{
+                "--value": "70",
+                "--size": "1.5rem",
+                "--thickness": "0.25rem",
+              }}
+            />
+          )}
+          {notes.map((note) => (
+            <Link key={note.id} href={`/note/${note.id}`}>
               <a>
-                <NoteCard title={note.title} />
+                <NoteCard note={note} />
               </a>
             </Link>
           ))}
@@ -47,8 +75,8 @@ const Dashboard: NextPage = () => {
       <div className={`modal ${!session && "modal-open"}`}>
         <div className="modal-box max-w-sm">
           <p>
-          You can have up to 100 notes with unlimited directories.
-          Sign in to unlock!
+            You can have up to 100 notes with unlimited directories. Sign in to
+            unlock!
           </p>
           <div className="modal-action">
             <Link href="/">
@@ -64,11 +92,14 @@ const Dashboard: NextPage = () => {
   );
 };
 
-const NoteCard = ({ title }: { title: string }) => {
+const NoteCard = ({ note }: { note: Note }) => {
   return (
-    <div className="card bg-base-100 border-2 border-base-300 cursor-pointer">
+    <div className="card card-compact bg-base-100 border-2 border-base-300 cursor-pointer">
       <div className="card-body">
-        <h2 className="card-title">{title}</h2>
+        <h2 className="card-title">{note.name}</h2>
+        <p className="opacity-50">
+          Modified {moment(note.updatedAt).fromNow()}
+        </p>
       </div>
     </div>
   );
